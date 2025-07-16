11d6d06b88040dc79ef5bc2714cd9c5845dfd928
LouisJanquart <louis.janquart@gmail.com>
2025-07-16 15:17:22 +0200

    refacto state

--- js/state.js
+++ js/state.js
@@ -2,20 +2,18 @@ export let currentStep = 1;
 export let currentAction = null;
 export let currentVisiteurId = null;
 
-export function setStep(step) {
+export const setStep = (step) => {
   currentStep = step;
-}
-
-export function setAction(action) {
+};
+export const setAction = (action) => {
   currentAction = action;
-}
-
-export function setVisiteurId(id) {
+};
+export const setVisiteurId = (id) => {
   currentVisiteurId = id;
-}
+};
 
-export function resetState() {
+export const resetState = () => {
   currentStep = 1;
   currentAction = null;
   currentVisiteurId = null;
-}
+};
